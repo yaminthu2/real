3f8a9b17a3551c7e1e80490e4b8dad099e58ac73
yaminthu2 <yaminthu641992@gmail.com>
2024-05-10 16:51:05 +0630

    may7.py

--- may7.py
+++ may7.py
@@ -1,45 +1,45 @@
 print("Have a good day!")
 num=[1,2,3,4,5]
-# for nums in num:
-#     if nums==3:
-#         print("found")
-#         continue
-#     print(nums)
+for nums in num:
+    if nums==3:
+        print("found")
+        continue
+    print(nums)
 
-# for nums in num:
-#     for letter in 'abcdef':
-#         print(nums,letter)
+for nums in num:
+    for letter in 'abcdef':
+        print(nums,letter)
 
-# for i in range(1,11):
-#     print(i)
+for i in range(1,11):
+    print(i)
 
-# x=0
-# while True:
-#     # if x==5:
-#     #     break
-#     print(x)
-#     x+=1
+x=0
+while True:
+    # if x==5:
+    #     break
+    print(x)
+    x+=1
 
-# def userName():
-#     print("Hey! everybody")
+def userName():
+    print("Hey! everybody")
 
-# userName()
+userName()
 
-# def hello_func(greeting,name):
-#     return '{} everyone,my name is {}'.format(greeting,name)
-# print(hello_func('hi','Yamin'))
-
-
-# def remark(name):
-#     return" {} Morning".format(name)
-# print(remark("Yamin"))
-
-# print(dir('name'))
+def hello_func(greeting,name):
+    return '{} everyone,my name is {}'.format(greeting,name)
+print(hello_func('hi','Yamin'))
 
 
-# def student_info(*args,**kwargs):
-#     print(args)
-#     print(kwargs)
+def remark(name):
+    return" {} Morning".format(name)
+print(remark("Yamin"))
+
+print(dir('name'))
 
 
-# student_info('Math','Art',name="Jone",age=33)
+def student_info(*args,**kwargs):
+    print(args)
+    print(kwargs)
+
+
+student_info('Math','Art',name="Jone",age=33)
