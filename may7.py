f30b5e48847ad0b1bd719c880b33c64a7363866d
yaminthu2 <yaminthu641992@gmail.com>
2024-05-10 16:47:07 +0630

    may7

---
+++ may7.py
@@ -0,0 +1,45 @@
+print("Have a good day!")
+num=[1,2,3,4,5]
+# for nums in num:
+#     if nums==3:
+#         print("found")
+#         continue
+#     print(nums)
+
+# for nums in num:
+#     for letter in 'abcdef':
+#         print(nums,letter)
+
+# for i in range(1,11):
+#     print(i)
+
+# x=0
+# while True:
+#     # if x==5:
+#     #     break
+#     print(x)
+#     x+=1
+
+# def userName():
+#     print("Hey! everybody")
+
+# userName()
+
+# def hello_func(greeting,name):
+#     return '{} everyone,my name is {}'.format(greeting,name)
+# print(hello_func('hi','Yamin'))
+
+
+# def remark(name):
+#     return" {} Morning".format(name)
+# print(remark("Yamin"))
+
+# print(dir('name'))
+
+
+# def student_info(*args,**kwargs):
+#     print(args)
+#     print(kwargs)
+
+
+# student_info('Math','Art',name="Jone",age=33)
